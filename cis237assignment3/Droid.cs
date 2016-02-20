a05a7767a0c01269e25651fdd9f686d612129b9d
Tim Keranen <tkeranen@mail.kvcc.edu>
2016-02-19 21:33:33 -0800

    Changed Droid class to an abstract class, and add backing fields, their properties, a default constructor, and an override constructor.

--- cis237assignment3/Droid.cs
+++ cis237assignment3/Droid.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace cis237assignment3
 {
-    class Droid
+    abstract class Droid
     {
+        protected string material;
+        protected string model;
+        protected string color;
+        protected decimal baseCost;
+        protected decimal totalCost;
+
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public Droid()
+        {
+        }
+
+        public override string ToString()
+        {
+            return this.material + " " + this.model + " " + this.color;
+        }
     }
 }
